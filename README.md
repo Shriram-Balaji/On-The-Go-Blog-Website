ebbd4ba7d4f915d12d4eb150406f65db4e21e04b
Shriram11596 <shrirambalaji1996@gmail.com>
2016-02-10 12:01:53 +0530

    Code
    Present code

--- README.md
+++
@@ -1,3 +0,0 @@
-# On-The-Go
-A blog website where users can share their thoughts and views  that runs on openshift .
-The site is hosted at www.onthego.co.in .
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION = array(); //resetting the session array to null.
-session_destroy();
-header('Location: index.php');
-exit;
-?>
