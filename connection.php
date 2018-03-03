d12855e9359eb060296cad12e1223b6e857dc70a
Shriram Balaji <>
2018-03-04 00:44:25 +0530

    replacing env variables

--- connection.php
+++ connection.php
@@ -1,21 +1,17 @@
 
-      <?php
+<?php
 try{
-
-        ini_set("display_errors", TRUE);
-
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST')); //ENSURES THE DB CONNECTION//
-define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
-define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
-
+ini_set("display_errors", TRUE);
+define('DB_HOST', getenv('DB_HOST')); //ENSURES THE DB CONNECTION//
+define('DB_PORT', getenv('DB_PORT')); 
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+define('DB_NAME', getenv('DB_NAME'));
 $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
 $db = new PDO($dsn, DB_USER, DB_PASS);
- $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }
 catch(Exception $e){
-
-	$e->getMessage();
+      $e->getMessage();
 }
 ?> 
