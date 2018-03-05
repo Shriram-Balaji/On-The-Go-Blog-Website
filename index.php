ef503e2deb1d8e25b9aa6f4e29026d8fe126879d
Shriram Balaji <>
2018-03-05 10:42:44 +0530

    Restyled landing page (wip)

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once('connection.php');
 <!--Copyright 2015
 Credit:
 Shriram Balaji   Nadeem Shaik
-St. Joseph's College Of Engineering
 -->
 <html lang="en">
   <head>
@@ -17,222 +16,229 @@ St. Joseph's College Of Engineering
     <meta charset="UTF-8">
     <title>On The Go Blog </title>
     <!--Import Google Icon Font-->
-    <link type="text/css" rel="stylesheet" href="css/login-materialize.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="node_modules/purecss/build/pure-min.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="node_modules/purecss/build/grids-core.css" />
+    <link type="text/css" rel="stylesheet" href="node_modules/purecss/build/grids-responsive-min.css" />
     <link type="text/css" rel="stylesheet" href="font/inter-ui/inter-ui.css" />
+    <link rel="stylesheet" type="text/css" href="font/circular/circular.css" />
     <link rel="stylesheet" type="text/css" href="css/loading.css" />
     <link rel="stylesheet" type="text/css" href="css/landing.css" />
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="js/classie.js"></script>
   </head>
   <body>
-    <script type="text/javascript">
-      //init code for facebook login starts here
-      (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+    <main class="pure-g">
+    <div id="background" class="">
+    </div>
+    <div class="container pure-g">
+      <script type="text/javascript">
+        //init code for facebook login starts here
+        (function (d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
 
 
-      window.fbAsyncInit = function () {
-        FB.init({
-          appId: '799680073475396',
-          cookie: true, // enable cookies to allow the server to access
-          // the session
-          xfbml: true, // parse social plugins on this page
-          version: 'v2.0' // use version 2.2
-        });
-
-
-        FB.getLoginStatus(function (response) {
-          statusChangeCallback(response);
-        });
-      };
-    </script>
-    <script type="text/javascript">
-      //login with facebook starts here
-      // Now that we've initialized the JavaScript SDK, we call
-      // FB.getLoginStatus().  This function gets the state of the
-      // person visiting this page and can return one of three states to
-      // the callback you provide.  They can be:
-      //
-      // 1. Logged into your app ('connected')
-      // 2. Logged into Facebook, but not your app ('not_authorized')
-      // 3. Not logged into Facebook and can't tell if they are logged into
-      //    your app or not.
-      //
-      // These three cases are handled in the callback function.
-      // Load the SDK asynchronously
-      function statusChangeCallback(response) {
-        console.log('statusChangeCallback');
-        console.log(response);
-        // The response object is returned with a status field that lets the
-        // app know the current login status of the person.
-        // Full docs on the response object can be found in the documentation
-        // for FB.getLoginStatus().
-        if (response.status === 'connected') {
-          // Logged into your app and Facebook.
-          testAPI();
-        } else if (response.status === 'not_authorized') {
-          // The person is logged into Facebook, but not your app.
-        } else {
-          // The person is not logged into Facebook, so we're not sure if
-          // they are logged into this app or not.
-
-        }
-      }
-      // This function is called when someone finishes with the Login
-      // Button.  See the onlogin handler attached to it in the sample
-      // code below.
-      function checkLoginState() {
-        FB.getLoginStatus(function (response) {
-          statusChangeCallback(response);
-        });
-      }
-      $(document).ready(function () {
-        $('#fblogin').click(function () {
-          FB.login(function (response) {
-            if (response.authResponse) {
-              testAPI(); //Get User Information.
-            } else {
-              alert('Authorization failed.');
-            }
-          }, {
-            scope: 'public_profile,email,user_birthday'
+        window.fbAsyncInit = function () {
+          FB.init({
+            appId: '799680073475396',
+            cookie: true, // enable cookies to allow the server to access
+            // the session
+            xfbml: true, // parse social plugins on this page
+            version: 'v2.0' // use version 2.2
           });
 
-        });
-        // Here we run a very simple test of the Graph API after login is
-        // successful.  See statusChangeCallback() for when this call is made.
-      });
-      function testAPI() {
-        console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me?fields=id,name,email,picture,first_name,birthday,last_name', function (response) {
-          var fblogin = $.ajax({
-            type: "POST",
-            data: response,
-            dataType: 'json',
-            url: 'facebook_login.php',
-            cache: false,
-          });
-          fblogin.done(function (data) {
-            window.top.location = "../home.php";
-          });
-          console.log('Successful login for: ' + response.name);
-        });
-      }
-      //
-    </script>
-    <div class="bg-img">
-    </div>
-    <div class="logo">
-      <div class="logo-title">
-        <img src="../img/logo.png" id="otglogo" />
-      </div>
-      <br>
-    </div>
-    <!-- Divisions for loading effects -->
-    <div class="la-anim-1"></div>
-    <!-- main container -->
-    <div class="modal" id="recovery">
-      <div class="modal-content"="rcvr_email">
-        <p> Please enter your registered email. We'll send the credentials required.</p>
-        <form>
-          EMAIL :
-          <input type="email" name="frgt_mail" id="frgt_mail">
-        </form>
 
-      </div>
-      <a href="#!" id="send" class="modal-action btn modal-close waves-effect waves">SEND</a>
-    </div>
-    <script type="text/javascript">
-      $('#send').click(function(event) {
-        /* Act on the event */
-        var email = $("#frgt_mail").val();
-        $.ajax({
-          url: 'recovery.php',
-          data: {
-            'email': email,
-            'update': 'password'
-          },
-          type: 'post',
-
-          cache: false,
-          success: function (data) {
-            Materialize.toast('Your details have been sent. Please check your mail!', 3000);
+          FB.getLoginStatus(function (response) {
+            statusChangeCallback(response);
+          });
+        };
+      </script>
+      <script type="text/javascript">
+        //login with facebook starts here
+        // Now that we've initialized the JavaScript SDK, we call
+        // FB.getLoginStatus().  This function gets the state of the
+        // person visiting this page and can return one of three states to
+        // the callback you provide.  They can be:
+        //
+        // 1. Logged into your app ('connected')
+        // 2. Logged into Facebook, but not your app ('not_authorized')
+        // 3. Not logged into Facebook and can't tell if they are logged into
+        //    your app or not.
+        //
+        // These three cases are handled in the callback function.
+        // Load the SDK asynchronously
+        function statusChangeCallback(response) {
+          console.log('statusChangeCallback');
+          console.log(response);
+          // The response object is returned with a status field that lets the
+          // app know the current login status of the person.
+          // Full docs on the response object can be found in the documentation
+          // for FB.getLoginStatus().
+          if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            testAPI();
+          } else if (response.status === 'not_authorized') {
+            // The person is logged into Facebook, but not your app.
+          } else {
+            // The person is not logged into Facebook, so we're not sure if
+            // they are logged into this app or not.
 
           }
-
-        });
-
-      });
-      $("#signup-tab").click(function (e) {
-        $("#signup-submit").click(function (e) {
-          console.log('done');
-          e.preventDefault();
-
-          var dataString = $("#signup_form").serialize();
-
-
-          var login = $.ajax({
-            url: "hello.php",
-            data: dataString,
-            type: "POST",
-            dataType: 'json',
-
-          });
-
-          login.done(function () {
-            console.log('logged in');
-
-            window.location.href = "index.php";
-
-
-          });
-
-        });
-
-
-      });
-      $('#login_submit').click(function (event) {
-        var username = $('#username').val();
-        var password = $('#password').val();
-        if (username.length <= 0 && password.length <= 0) {
-          alert("empty");
         }
-        /* Act on the event */
-      });
-      $('#fblogin').click(function () {
-        $(document).ajaxComplete(function () {
-          $('.la-anim-1').show();
-          setTimeout(function () {
-            $('.la-anim-1').hide();
-            window.location.href = "home.php";
-          }, 1000 * 0.5);
+        // This function is called when someone finishes with the Login
+        // Button.  See the onlogin handler attached to it in the sample
+        // code below.
+        function checkLoginState() {
+          FB.getLoginStatus(function (response) {
+            statusChangeCallback(response);
+          });
+        }
+        $(document).ready(function () {
+          $('#fblogin').click(function () {
+            FB.login(function (response) {
+              if (response.authResponse) {
+                testAPI(); //Get User Information.
+              } else {
+                alert('Authorization failed.');
+              }
+            }, {
+              scope: 'public_profile,email,user_birthday'
+            });
+
+          });
+          // Here we run a very simple test of the Graph API after login is
+          // successful.  See statusChangeCallback() for when this call is made.
+        });
+        function testAPI() {
+          console.log('Welcome!  Fetching your information.... ');
+          FB.api('/me?fields=id,name,email,picture,first_name,birthday,last_name', function (response) {
+            var fblogin = $.ajax({
+              type: "POST",
+              data: response,
+              dataType: 'json',
+              url: 'facebook_login.php',
+              cache: false,
+            });
+            fblogin.done(function (data) {
+              window.top.location = "../home.php";
+            });
+            console.log('Successful login for: ' + response.name);
+          });
+        }
+        //
+      </script>
+      <div class="logo pure-u-1 pure-u-lg">
+        <div class="logo-title">
+          <p>On The Go</p>
+          <p class="logo-subheading no-margin">Publish Articles. Online. </p>
+        </div>
+      </div>
+      <!-- Divisions for loading effects -->
+      <div class="la-anim-1"></div>
+      <!-- main container -->
+
+      <div class="login-panel pure-u-1 pure-u-md-1-3">
+        <div id="fb-container">
+          <button id='fblogin' class='btn pure-button'>Sign In with Facebook</button>
+        </div>
+        <div id="gp-container">
+          <button id='gplogin' class='btn pure-button'>Sign In with Google</button>
+        </div>
+      </div>
+    
+      <script type="text/javascript">
+        $('#send').click(function(event) {
+          /* Act on the event */
+          var email = $("#frgt_mail").val();
+          $.ajax({
+            url: 'recovery.php',
+            data: {
+              'email': email,
+              'update': 'password'
+            },
+            type: 'post',
+
+            cache: false,
+            success: function (data) {
+              Materialize.toast('Your details have been sent. Please check your mail!', 3000);
+
+            }
+
+          });
 
         });
-      });
-      //frgt passwd
-      // $('.modal-trigger').leanModal();
-      //preloading top bar
-      var inProgress = false;
-      Array.prototype.slice.call(document.querySelectorAll('#la-buttons > a')).forEach(function (el, i) {
-        var anim = el.getAttribute('data-anim'),
-          animEl = document.querySelector('.' + anim);
-        el.addEventListener('click', function () {
-          if (inProgress) return false;
-          inProgress = true;
-          classie.add(animEl, 'la-animate');
-          setTimeout(function () {
-            classie.remove(animEl, 'la-animate');
-            inProgress = false;
-          }, 6000);
+        $("#signup-tab").click(function (e) {
+          $("#signup-submit").click(function (e) {
+            console.log('done');
+            e.preventDefault();
+
+            var dataString = $("#signup_form").serialize();
+
+
+            var login = $.ajax({
+              url: "hello.php",
+              data: dataString,
+              type: "POST",
+              dataType: 'json',
+
+            });
+
+            login.done(function () {
+              console.log('logged in');
+
+              window.location.href = "index.php";
+
+
+            });
+
+          });
+
+
         });
-      });
-    </script>
-    <script type="text/javascript" ></script>
+        $('#login_submit').click(function (event) {
+          var username = $('#username').val();
+          var password = $('#password').val();
+          if (username.length <= 0 && password.length <= 0) {
+            alert("empty");
+          }
+          /* Act on the event */
+        });
+        $('#fblogin').click(function () {
+          $(document).ajaxComplete(function () {
+            $('.la-anim-1').show();
+            setTimeout(function () {
+              $('.la-anim-1').hide();
+              window.location.href = "home.php";
+            }, 1000 * 0.5);
+
+          });
+        });
+        //frgt passwd
+        // $('.modal-trigger').leanModal();
+        //preloading top bar
+        var inProgress = false;
+        Array.prototype.slice.call(document.querySelectorAll('#la-buttons > a')).forEach(function (el, i) {
+          var anim = el.getAttribute('data-anim'),
+            animEl = document.querySelector('.' + anim);
+          el.addEventListener('click', function () {
+            if (inProgress) return false;
+            inProgress = true;
+            classie.add(animEl, 'la-animate');
+            setTimeout(function () {
+              classie.remove(animEl, 'la-animate');
+              inProgress = false;
+            }, 6000);
+          });
+        });
+      </script>
+      <script type="text/javascript" ></script>
+    </div>
+    </main>
+
   </body>
 </html>
--- new-post.php
+++ new-post.php
@@ -21,140 +21,147 @@ else
 
 <!doctype html>
 <html>
-  
-   <head>
+
+<head>
 
 
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          
-    <meta charset="utf-8">
-    <meta name="description" content="grande.js: a way for you to express yourself">
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <meta charset="utf-8">
+  <meta name="description" content="grande.js: a way for you to express yourself">
 
 
-    <link href = "css/article-editor.css" rel="stylesheet">
-       <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      
-      <link type="text/css" rel="stylesheet" href="css/login-materialize.css"  media="screen,projection"/>
+  <link href="css/article-editor.css" rel="stylesheet">
+  <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-    <link href="css/menu.css" rel="stylesheet"/>
-    <link href="css/editor.css" rel="stylesheet"/>
-            <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-  </head>
+  <link type="text/css" rel="stylesheet" href="css/login-materialize.css" media="screen,projection" />
 
-  <body class="g-body">
-        <a class = "close btn tooltipped" href = "home.php"id = "discard" data-position="left" data-delay="50" data-tooltip="Discard"> <i class="material-icons">close</i></a>
+  <link href="css/menu.css" rel="stylesheet" />
+  <link href="css/editor.css" rel="stylesheet" />
+  <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+</head>
 
-    <section>
+<body class="g-body">
+  <a class="close btn tooltipped" href="home.php" id="discard" data-position="left" data-delay="50" data-tooltip="Discard">
+    <i class="material-icons">close</i>
+  </a>
 
-    <form class = "new-article" method="POST">
-  
-     <div class="input-field col s12 ">
-    <select class="browser-default" id = "category" name="category" required> 
-
-      <option value="1">Technology</option>
-      <option value="2">Design</option>
-      <option value="3">Entertainment</option>
-      <option value="4">Travel</option>
-      <option value="5">Fashion</option>
-   </select>
-
-  </div>
-<div class="row">
+  <section>
+    <form class="new-article" method="POST">
+      <div class="input-field col s12 ">
+        <select class="browser-default" id="category" name="category" required>
+          <option value="1">Technology</option>
+          <option value="2">Design</option>
+          <option value="3">Entertainment</option>
+          <option value="4">Travel</option>
+          <option value="5">Fashion</option>
+        </select>
+      </div>
+      <div class="row">
         <div class="input-field col s12">
-          <input type = "text" id="title"  name = "title" class="materialize-textarea" required />
+          <input type="text" id="title" name="title" class="materialize-textarea" required />
           <label for="description">Title</label>
         </div>
       </div>
-       <div class="row">
+      <div class="row">
         <div class="input-field col s12">
-          <textarea input type = "text" id="description"  name = "description" class="materialize-textarea" length = "400" required></textarea>
+          <textarea input type="text" id="description" name="description" class="materialize-textarea" length="400" required></textarea>
           <label for="description">Description</label>
         </div>
       </div>
-      <article class="content" name = "article">
-       <div> <p class="welcome-text">Welcome! Please read the <b>instructions below</b> and start typing your article highlighted below .</p></div>
-       <p class="focus" > </p>
-
-     
-
-       </article>
-       </form>
-      <a class="waves-effect waves-light btn" id = "submit">Publish</a>
-    </section>
-
-<div class = "instructions">
-<h4>INSTRUCTIONS</h4>
-<p>Select or highlight text to change formatting, add headers.</p>
-        <ul>
-          <li>Create a bulleted list simply by typing "- " or "* " and hitting enter</li>
-        
-          <hr contenteditable="false">
-          <li><i>Press Enter Twice </i> to create a divider to separate your content.</li>
-        <hr contenteditable="false">
-        </ul>
-
-</div>
-    <script type="text/javascript" src="js/grande.js"></script>
-    <script type="text/javascript">
-      grande.bind(document.querySelectorAll(".focus"));
-
-  $(document).ready(function() {
-    $('select').material_select();
-  });
-
-$(".content").mouseenter(function(event) {
-  $('span').removeAttr('style');
-  /* Act on the event */
-  $('.focus').focus();
-});
-
-$(".focus").bind('paste input' , function(e) {
-  /* Act on the event */
-$('*').removeAttr('style');
+      <article class="content" name="article">
+        <div>
+          <p class="welcome-text">Welcome! Please read the
+            <b>instructions below</b> and start typing your article highlighted below .</p>
+        </div>
+        <p class="focus"> </p>
 
 
 
-});
-$("#submit").one('click',function(){
+      </article>
+    </form>
+    <a class="waves-effect waves-light btn" id="submit">Publish</a>
+  </section>
 
-  
-    console.log('done');
+  <div class="instructions">
+    <h4>INSTRUCTIONS</h4>
+    <p>Select or highlight text to change formatting, add headers.</p>
+    <ul>
+      <li>Create a bulleted list simply by typing "- " or "* " and hitting enter</li>
 
-$('.welcome-text').remove();
-var category = $('#category option:selected').text();
-var description = $('#description').val();
-var title = $('#title').val();
-var article = $('.content').html();
-console.log(article);
-  
-   var submit = $.ajax({ 
-     url: "publish_article.php",
-     data :{'category': category,'description': description,'title' : title,'article' : article},
-     type: "POST",
-    
-});
+      <hr contenteditable="false">
+      <li>
+        <i>Press Enter Twice </i> to create a divider to separate your content.</li>
+      <hr contenteditable="false">
+    </ul>
 
-submit.done(function(){
+  </div>
+  <script type="text/javascript" src="js/grande.js"></script>
+  <script type="text/javascript">
+    grande.bind(document.querySelectorAll(".focus"));
 
-console.log('blogged in');
- Materialize.toast('Your Article has been published!', 2000);
+    $(document).ready(function () {
+      $('select').material_select();
+    });
+
+    $(".content").mouseenter(function (event) {
+      $('span').removeAttr('style');
+      /* Act on the event */
+      $('.focus').focus();
+    });
+
+    $(".focus").bind('paste input', function (e) {
+      /* Act on the event */
+      $('*').removeAttr('style');
 
 
-  setTimeout(function(){
+
+    });
+    $("#submit").one('click', function () {
 
 
-window.location.href = "../home.php";
+      console.log('done');
 
-  },1000*3);
-});
-});
+      $('.welcome-text').remove();
+      var category = $('#category option:selected').text();
+      var description = $('#description').val();
+      var title = $('#title').val();
+      var article = $('.content').html();
+      console.log(article);
 
-document.documentElement.style.overflowX = 'hidden !important';
-    </script>
+      var submit = $.ajax({
+        url: "publish_article.php",
+        data: {
+          'category': category,
+          'description': description,
+          'title': title,
+          'article': article
+        },
+        type: "POST",
 
-  </body>
+      });
+
+      submit.done(function () {
+
+        console.log('blogged in');
+        Materialize.toast('Your Article has been published!', 2000);
+
+
+        setTimeout(function () {
+
+
+          window.location.href = "../home.php";
+
+        }, 1000 * 3);
+      });
+    });
+
+    document.documentElement.style.overflowX = 'hidden !important';
+  </script>
+
+</body>
 
 </html>
